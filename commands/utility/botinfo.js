b520b378d9573d0bfa08dc2bb719125d340917de
Jacob Holman <69821200+JacobHolman@users.noreply.github.com>
2025-08-01 00:27:21 -0400

    Remove uptime

--- commands/utility/botinfo.js
+++ commands/utility/botinfo.js
@@ -3,23 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const { getColor } = require('../../misc/colorUtil');
 
-function formatUptime(seconds) {
-  const days = Math.floor(seconds / 86400);
-  seconds %= 86400;
-  const hours = Math.floor(seconds / 3600);
-  seconds %= 3600;
-  const minutes = Math.floor(seconds / 60);
-  seconds %= 60;
-
-  const parts = [];
-  if (days > 0) parts.push(`${days}d`);
-  if (hours > 0) parts.push(`${hours}h`);
-  if (minutes > 0) parts.push(`${minutes}m`);
-  parts.push(`${seconds}s`);
-
-  return parts.join(' ');
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('botinfo')
@@ -36,10 +19,6 @@ module.exports = {
 
       const rawData = fs.readFileSync(filePath, 'utf-8');
       const info = JSON.parse(rawData);
-
-      const uptimeMillis = Date.now() - interaction.client.startTime;
-      const uptimeSeconds = Math.floor(uptimeMillis / 1000);
-      const uptimeStr = formatUptime(uptimeSeconds);
 
       const commandsCount = interaction.client.commands?.size ?? 0;
 
@@ -58,7 +37,6 @@ module.exports = {
           { name: 'Version', value: info.version || 'N/A', inline: true },
           { name: 'Developers', value: developersStr, inline: true },
           { name: 'Commands', value: commandsCount.toString(), inline: true },
-          { name: 'Uptime', value: uptimeStr, inline: true },
           { name: 'GitHub', value: info.github ? `[Link](${info.github})` : 'N/A', inline: true }
         )
         .setTimestamp()
